3181387c942bef51fc5a5836337f9c49c04d9dcf
Mahadev1729 <devcampus17@gmail.com>
2025-08-29 22:50:41 +0530

    refactor: improve Flask API setup and UserModel definition; add database creation script

--- api.py
+++ api.py
@@ -1,25 +1,23 @@
 ﻿from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL']="sqlite:///database.db"
-db=SQLAlchemy(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-class UserModel(db.model):
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(80),unique=True,nullable=False)
-    email=db.Column(db.String(80),unique=True,nullable=False)
+db = SQLAlchemy(app)
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
     
     def __repr__(self):
-        return f"User(name={self.name},email={self.name})"
-    
-    
-    
-    
+        return f"User(name={self.name}, email={self.email})"
+
 @app.route('/')
 def home():
     return '<h1>Flask REST API</h1>'
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
-    
